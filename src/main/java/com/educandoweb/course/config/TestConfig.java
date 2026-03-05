90d6d7b915e783f71638b0aa71746776fbe24788
Marcelo Sarinho <marceloactsarinho@gmail.com>
2026-03-05 14:46:16 -0300

    feat: service layer, component registration

--- src/main/java/com/educandoweb/course/config/TestConfig.java
+++ src/main/java/com/educandoweb/course/config/TestConfig.java
@@ -13,13 +13,13 @@ import java.util.Arrays;
 @Profile("test")
 public class TestConfig implements CommandLineRunner {
     @Autowired
-    private UserRepository userRepository;
+    private UserRepository repository;
 
     @Override
     public void run(String... args) throws Exception {
         User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
         User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 
-        userRepository.saveAll(Arrays.asList(u1, u2));
+        repository.saveAll(Arrays.asList(u1, u2));
     }
 }
